902e355c6faf1ec539f2a43a209426298a8cd79c
abdullah-zzz <ak0344494@gmail.com>
2025-07-17 20:25:46 +0500

    admin panel changes

--- controllers/user_controllers.js
+++ controllers/user_controllers.js
@@ -233,14 +233,14 @@ const loginUser = async (req, res) => {
     res.cookie("refresh_token", refreshToken, {
       httpOnly: true,
       secure: process.env.NODE_ENV === "production",
-      sameSite: "strict",
+      sameSite: "None",
       maxAge: 7 * 24 * 60 * 60 * 1000
     });
 
     res.cookie("access_token", accessToken, {
       httpOnly: true,
       secure: process.env.NODE_ENV === "production",
-      sameSite: "strict",
+      sameSite: "None",
       maxAge: 15 * 60 * 1000
     });
 
